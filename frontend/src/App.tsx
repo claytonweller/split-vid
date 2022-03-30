da2405422a1eb41301274e5de1d729475289bc46
ClaytonWeller <clayton.weller@gmail.com>
2022-03-30 11:40:16 -0600

    Pause video by timeCode
    This will allow time for the choose your own
    adventure moments.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
 export default function App() {
+  const [videoTime, setVideoTime] = useState(0)
+  const handleTime = (e: any) => {
+    const { currentTime } = e.target
+    if (currentTime > 4) e.target.pause()
+    setVideoTime(currentTime)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         Split-Vid
-        <video src="http://localhost:5000/videoId/" width="500px" height="auto" controls autoPlay id="videoPlayer"></video>
+        <div>TimeStamp: {videoTime}</div>
+        <div>
+          <button style={{ position: 'relative', top: 200, left: 0, width: '20%' }}>PICK</button>
+          <video onTimeUpdate={(handleTime)} src="http://localhost:5000/videoId/" width="500px" height="auto" controls id="videoPlayer"></video>
+        </div>
       </header>
     </div>
   );
