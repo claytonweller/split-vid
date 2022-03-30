4d67b9077d6bc3821f138a63398e75c6bbbdca51
ClaytonWeller <clayton.weller@gmail.com>
2022-03-30 12:05:12 -0600

    Button appears on based upon timeCode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,20 @@ import './App.css';
 
 export default function App() {
   const [videoTime, setVideoTime] = useState(0)
+  const [buttonIsVisible, setButtonIsVisible] = useState(false)
   const handleTime = (e: any) => {
     const { currentTime } = e.target
-    if (currentTime > 4) e.target.pause()
+    if (currentTime > 4) {
+      e.target.pause()
+      setButtonIsVisible(true)
+    }
     setVideoTime(currentTime)
+  }
+  const buttonStyle = {
+    width: '20%',
+    zIndex: 10,
+    bottom: 50,
+    display: buttonIsVisible ? 'inline' : 'none'
   }
 
   return (
@@ -15,9 +25,16 @@ export default function App() {
       <header className="App-header">
         Split-Vid
         <div>TimeStamp: {videoTime}</div>
-        <div>
-          <button style={{ position: 'relative', top: 200, left: 0, width: '20%' }}>PICK</button>
-          <video onTimeUpdate={(handleTime)} src="http://localhost:5000/videoId/" width="500px" height="auto" controls id="videoPlayer"></video>
+        <div style={{ position: 'relative' }}>
+          <video
+            style={{ display: 'block' }}
+            onTimeUpdate={(handleTime)}
+            src="http://localhost:5000/videoId/"
+            width="500px" height="auto"
+            controls
+            id="videoPlayer"
+          />
+          <button style={{ ...buttonStyle, position: 'absolute' }}>PICK</button>
         </div>
       </header>
     </div>
